8441b346440c01fab41ffd203e5dc9267dc70653
yettol-tools <egonzalez209@gmail.com>
2022-11-07 13:37:26 -0600

    add  class container to main

--- next-js-13-app/app/page.jsx
+++ next-js-13-app/app/page.jsx
@@ -1,3 +1,8 @@
+import style from '../styles/Home.module.css';
 export default function Index() {
-  return <div>asdf</div>;
+  return (
+    <main className={style.container}>
+      <h1>Home</h1>
+    </main>
+  );
 }
